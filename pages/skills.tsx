3bc3ef3f5b283f0b7e0ed5e76f69da9670c853ac
Strixyl <justerureta07@gmail.com>
2025-08-16 15:46:03 +0800

    minor clean up

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { SiR, SiPython, SiNextdotjs } from 'react-icons/si';
@@ -79,5 +78,8 @@ export default function Skills() {
         </div>
     )
 }
+
+
+
 
 
